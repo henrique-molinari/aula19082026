c72d52825fa35fa246537fbf72da20c1f49c4483
Henrique Molinari <henrique.molinari@sou.unifeob.edu.br>
2026-08-19 21:55:03 -0300

    Configuração do bd

--- pj_pet_get/server.js
+++ pj_pet_get/server.js
@@ -1,1 +1,26 @@
-// 
+// Requere o express
+const express = require('express')
+
+//instancia do express
+const api = express()
+
+// Requere o cors
+const cors = require('cors')
+
+//Requere a conexão
+const conn = require('./db/conn')
+
+// Configurando JSON response - Para ler e devolver um JSON
+api.use(express.json)
+
+// salve Cors
+api.use(cors({ credential: true, origin: 'http://localhost:5000' }))
+
+
+conn.sync()
+    .then(() => { api.listen(5000) })//
+    .catch(error=>{console.info(error)})
+// Start da API
+// api.listen(5000, () => { 
+//     console.log('Servidor Inicializado')
+// })
